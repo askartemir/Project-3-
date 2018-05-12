7b8a46a0ded5f308afe2fad19cf4d2676e98f109
Rebecca B <rebeccabrown1234@gmail.com>
2018-05-12 13:47:01 -0400

    react compiles!

--- client/artist-react/src/components/modal/modal.js
+++ client/artist-react/src/components/modal/modal.js
@@ -1,17 +1,22 @@
 import React from "react"
-import Form from "./form.js"
+import Form from "../form/form.js"
 
 const Modal = props => (
-	  <div id="modal1" class="modal">
-    <div className="modal-content">
+<div id="modal1" class="modal">
 
-    <Form />
+	<div className="modal-content">
 
-    <div class="modal-footer">
-      <a href="#!" class="modal-close waves-effect waves-green btn-flat"></a>
-      	<a class="waves-effect waves-light btn">Go</a>
-    </div>
-  </div>
+	    <Form />
+
+	    <div class="modal-footer">     
+		    <a href="#!" class="modal-close waves-effect waves-green btn-flat"></a>
+			<a class="waves-effect waves-light btn">Go</a>
+	    </div>
+
+	</div>
+
+</div>
+
 )
 
 export default Modal;
