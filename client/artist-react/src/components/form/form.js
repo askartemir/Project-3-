5f7d85fd974e66377c807927cb5be1ee52f93b81
Rebecca B <rebeccabrown1234@gmail.com>
2018-05-12 13:20:18 -0400

    added react, updated css

---
+++ client/artist-react/src/components/form/form.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Form = props => (
+	<div className="card-content">
+        <span className ="card-title"><h3>Sign Up</h3></span>
+        <div className="row">
+          <div className="input-field col s12">
+            <label for="email">Email address</label>
+            <input type="email" name="email" id="email" />
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="input-field col s12">
+            <label for="password">Password </label>
+            <input type="password" name="password" id="password" />
+          </div>
+        </div>
+      </div>
+	)
+
+export default Form;
