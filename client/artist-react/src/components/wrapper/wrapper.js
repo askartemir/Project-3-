f437e4554bd6a3dc88f7baa52aa0de3816827b04
Rebecca B <rebeccabrown1234@gmail.com>
2018-05-14 18:44:42 -0400

    added components

--- client/artist-react/src/components/wrapper/wrapper.js
+++ client/artist-react/src/components/wrapper/wrapper.js
@@ -1,2 +1,15 @@
 import React from "react";
+import { Card } from "react-materialize"
 import Profile from "./profile.js"
+import "./user.css"
+
+const Wrapper = props => (
+	<Card className='small'
+  header={<CardTitle image='img/sample-1.jpg'>Card Title</CardTitle>}
+  actions={[<a href='#'>This is a Link</a>]}>
+  I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.
+</Card>
+	)
+
+
+export default Wrapper;
