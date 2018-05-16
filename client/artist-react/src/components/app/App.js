855612734df97d3ccd558e1812b3301c53032265
Rebecca B <rebeccabrown1234@gmail.com>
2018-05-16 19:38:32 -0400

    the final react push

--- client/artist-react/src/components/app/App.js
+++ client/artist-react/src/components/app/App.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
 import './App.css';
-import Login from "../../pages/login";
+
+import Login from "../../pages/login.js";
+import Home from "../../pages/home.js";
+
+import Form from "./form/form.js";
+import Modal from "./modal/modal.js";
+import Image from "./image/image.js";
+import Profile from "./profile/profile.js";
+import TextArea from "./textarea/textarea.js";
+import Wrapper from "./wrapper/wrapper.js";
 
 class App extends Component {
   render() {
