5b1f6c559bd13c75c2a3a526ac6157fa8f001b76
Rebecca B <rebeccabrown1234@gmail.com>
2018-05-15 21:26:45 -0400

    updated routes

--- client/artist-react/src/reactroutes/loginroute.js
+++ client/artist-react/src/reactroutes/loginroute.js
@@ -3,10 +3,12 @@ import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Login from "../pages/login.js";
 import Form from "../components/form/form.js";
 
+// Demo page for the moment till I re-write these
+
 const LoginRoute = () => (
   <Router>
     <div>
-      <Route exact path="/home" component={Home} />
+      <Route exact path="/hello" component={Login} />
     </div>
   </Router>
 );
