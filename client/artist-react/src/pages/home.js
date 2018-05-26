4b13f2f69ee1cafb5f126b4427d7f666a17a9bc1
Aaron Bugden <aaronbugden@gmail.com>
2018-05-26 14:06:19 -0400

    Home and Login Changes

--- client/artist-react/src/pages/home.js
+++ client/artist-react/src/pages/home.js
@@ -6,18 +6,19 @@ import "./home.css";
 import TextArea from "../components/textarea/textarea";
 import DropDown from "../components/profile/dropdown";
 // import Paginate from "../components/pagination/pagination";
+import "./login.js";
 
 export default class Home extends React.Component {
 	render () {
 		return (
 		<div>
-			<div className = "wrapper">
-				<Card className='small'
-				  header={<CardTitle image='https://upload.wikimedia.org/wikipedia/en/thumb/6/60/Kratos_PS4.jpg/234px-Kratos_PS4.jpg'>Card Title</CardTitle>}
+			{/* <div className = "wrapper">
+				<Card className='small' id = "profile-card"
+				  header={<CardTitle image="http://via.placeholder.com/140x140">Card Title</CardTitle>}
 				  actions={[<a href='#'>This is a Link</a>]}>
 				  I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.
 				</Card>
-			</div>
+			</div> */}
 
 			<DropDown />
 			{/* <Wrapper /> */}
